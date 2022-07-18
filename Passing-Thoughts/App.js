ba905cdf176360055c9cc3962f84796b727897e3
hambalee <hambalee@outlook.com>
2022-07-18 22:38:25 +0700

    update Passing Thoughts with completed task

--- Passing-Thoughts/App.js
+++ Passing-Thoughts/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import { AddThoughtForm } from './AddThoughtForm';
-import { Thought } from './Thought';
-import { generateId, getNewExpirationTime } from './utilities';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { AddThoughtForm } from "./AddThoughtForm";
+import { Thought } from "./Thought";
+import { generateId, getNewExpirationTime } from "./utilities";
 
 function App() {
   const [thoughts, setThoughts] = useState([
     {
       id: generateId(),
-      text: 'This is a place for your passing thoughts.',
+      text: "This is a place for your passing thoughts.",
       expiresAt: getNewExpirationTime(),
     },
     {
@@ -18,16 +18,26 @@ function App() {
     },
   ]);
 
+  const addThought = (thought) => {
+    setThoughts((prev) => [thought, ...prev]);
+  };
+
+  const removeThought = (thoughtIdToRemove) => {
+    setThoughts((prev) =>
+      prev.filter((thought) => thought.id !== thoughtIdToRemove)
+    );
+  };
+
   return (
     <div className="App">
       <header>
         <h1>Passing Thoughts</h1>
       </header>
       <main>
-        <AddThoughtForm />
+        <AddThoughtForm addThought={addThought}/>
         <ul className="thoughts">
           {thoughts.map((thought) => (
-            <Thought key={thought.id} thought={thought} />
+            <Thought key={thought.id} thought={thought} removeThought={removeThought} />
           ))}
         </ul>
       </main>
@@ -35,4 +45,4 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ Passing-Thoughts/Thought.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+
+export function Thought(props) {
+  const { thought, removeThought } = props;
+
+  useEffect(() => {
+    const timeRemaining = thought.expiresAt - Date.now();
+    const timeoutID = setTimeout(() => {
+      removeThought(thought.id);
+    },timeRemaining)
+    return () => clearTimeout(timeoutID)
+  }, [thought])
+
+  const handleRemoveClick = () => {
+    removeThought(thought.id);
+  };
+
+  return (
+    <li className="Thought">
+      <button
+        aria-label="Remove thought"
+        className="remove-button"
+        onClick={handleRemoveClick}
+      >
+        &times;
+      </button>
+      <div className="text">{thought.text}</div>
+    </li>
+  );
+}
