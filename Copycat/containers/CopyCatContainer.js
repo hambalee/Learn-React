e6fda554227c44d40abed3e0f078e68d711b4553
hambalee <hambalee@outlook.com>
2022-07-20 00:14:17 +0700

    add Copycat

---
+++ Copycat/containers/CopyCatContainer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { CopyCat } from "../components/CopyCat";
+
+class CopyCatContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      copying: true,
+      input: "",
+    };
+
+    this.toggleTape = this.toggleTape.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  toggleTape() {
+    this.setState({ copying: !this.state.copying });
+  }
+
+  handleChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  render() {
+    const copying = this.state.copying;
+    const toggleTape = this.toggleTape;
+    const value = this.state.input;
+    const handleChange = this.handleChange;
+
+    return (
+      <CopyCat
+        copying={copying}
+        toggleTape={toggleTape}
+        value={value}
+        handleChange={handleChange}
+        name={'Alice'}
+      />
+    );
+  }
+}
+
+ReactDOM.render(<CopyCatContainer />, document.getElementById("app"));
